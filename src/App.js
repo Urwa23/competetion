f649359cc169203f6c1e83df9f8056cbf2e4f125
nimraahmed <nimra1408@gmail.com>
2022-07-19 14:58:42 +0500

    appbar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import Header from "./components/header";
+import Card from "./components/Card";
 
 function App() {
-  return (
-    <div className="App">
-      <h1>hello world</h1>
-    </div>
-  );
+	return (
+		<div className="App">
+			<Header />
+			<h1>hello world</h1>
+			<Card />
+		</div>
+	);
 }
 
 export default App;
---
+++ src/components/header.js
@@ -0,0 +1,12 @@
+import React from "react";
+import PrimarySearchAppBar from "./AppBar";
+
+const Header = () => {
+	return (
+		<div>
+			<PrimarySearchAppBar />
+		</div>
+	);
+};
+
+export default Header;
