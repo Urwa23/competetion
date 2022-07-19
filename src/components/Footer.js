d62eff81db89404c56601c8c6fb96226209a6e54
Yumna Zahid <yumnazahid14@gmail.com>
2022-07-19 15:38:46 +0500

    footer

---
+++ src/components/Footer.js
@@ -0,0 +1,117 @@
+import react from "react";
+// <!-- import "../styling/footer.css"; -->
+import "../styling/Footer.css";
+
+function Footer() {
+    	return (
+		<div style={{ backgroundColor: "#1E2641" }}>
+            <div className="footer" style={{ width: "100%", padding: "15px"}} >
+            <div className="footer-text-left">
+					<ul style={{ listStyleType: "none" }}>
+						<li style={{ fontWeight: "bold", fontSize:"15px"}}>INFO</li>
+						<li>Formats</li>
+						<li>Compression</li>
+						<li>Pricing</li>
+						<li>FAQ</li>
+						<li>Status</li>
+					</ul>
+            </div>
+			<div className="footer-text-left">
+					<ul style={{ listStyleType: "none" }}>
+						<li style={{ fontWeight: "bold" }}>RESOURCES</li>
+						<li>Photographers</li>
+						<li>Tools</li>
+						<li>Blog</li>
+						
+					</ul>
+			</div>
+			<div className="footer-text-left">
+					<ul style={{ listStyleType: "none"}}>
+						<li style={{ fontWeight: "bold" }}>COMPANY</li>
+						<li>AboutUs</li>
+						<li>Sustainability</li>
+						<li>Terms</li>
+						<li>Privacy</li>
+					
+					</ul>
+			</div>
+			<div></div>
+
+			</div>
+		</div>
+		)
+		}
+
+export default Footer;
+
+
+
+
+            {/* <!-- </div>
+            </div>
+			
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            <div className="footer">
+				<div style={{ width: "30%", padding: "20px" }}>
+					â€œTell me and I forget. Teach me and I remember. Involve me and I
+					learn.â€
+				</div>
+				<div>
+					<ul style={{ listStyleType: "none" }}>
+						<li style={{ fontWeight: "bold" }}>Learn</li>
+						<li>Explore Classes</li>
+						<li>Learner Allowance</li>
+						<li>FitChild for Employers</li>
+						<li>Financial Assistance</li>
+						<li>Schools and Districts</li>
+						<li>Give a Gift Card</li>
+					</ul>
+				</div>
+				<div>
+					<ul style={{ listStyleType: "none" }}>
+						<li style={{ fontWeight: "bold" }}>Teach</li>
+						<li>Independent Teachers</li>
+						<li>Organizations</li>
+					</ul>
+				</div>
+				<div>
+					<ul style={{ listStyleType: "none" }}>
+						<li style={{ fontWeight: "bold" }}>About</li>
+						<li>Company</li>
+						<li>Careers</li>
+						<li>Blog</li>
+						<li>Press</li>
+					</ul>
+				</div>
+				<div>
+					<ul style={{ listStyleType: "none" }}>
+						<li style={{ fontWeight: "bold" }}>Support</li>
+						<li>Help</li>
+						<li>Safety</li>
+						<li>Privacy</li>
+						<li>Learner Privacy</li>
+						<li>Terms</li>
+					</ul>
+				</div>
+			</div>
+		</div>
+	);
+}
+
+export default Footer; --> */}
